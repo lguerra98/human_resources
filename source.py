53bc41ae0043223a054f6d7ee82a144840a5901e
Lukas <guerra0815@gmail.com>
2024-03-14 22:59:46 -0500

    comentarios y correcion commit anterior

--- source.py
+++ source.py
@@ -16,6 +16,29 @@ from sklearn.compose import ColumnTransformer
 from imblearn.over_sampling import RandomOverSampler
 from imblearn.under_sampling import RandomUnderSampler
 from sklearn.svm import SVC
+from sklearn.metrics import f1_score
+
+from sklearn.feature_selection import VarianceThreshold, SelectKBest, SelectFromModel, chi2, mutual_info_classif, RFECV, RFE
+from sklearn.linear_model import Lasso, LassoCV, RidgeClassifier, RidgeClassifierCV, LinearRegression, LogisticRegression, LinearRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, OrdinalEncoder, MinMaxScaler
+from sklearn.compose import ColumnTransformer
+from imblearn.over_sampling import RandomOverSampler
+from imblearn.under_sampling import RandomUnderSampler
+import numpy as np
+
+from sklearn.feature_selection import VarianceThreshold, SelectKBest, SelectFromModel, chi2, mutual_info_classif, RFECV, RFE
+from sklearn.linear_model import Lasso, LassoCV, RidgeClassifier, RidgeClassifierCV, LinearRegression, LogisticRegression, LinearRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler, OneHotEncoder, OrdinalEncoder, MinMaxScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.metrics import precision_score, f1_score
+
 
 from collections import Counter
 import itertools
@@ -297,110 +320,8 @@ def barcharts(df=..., normalize=True, obj_col="Attrition", rotation=90, only=Fal
     plt.tight_layout()
 
     
-
-def feature_sel(num_feat_kbest=20, num_rfe=15, main_metric="chi2", plot=False):
     
-    query = "SELECT * FROM df"
-    df = create_df(query, index=True)
-    
-    target = "Attrition"
-    
-    X = df.drop([target, "retirementType", "resignationReason", "Date"], axis=1)
-    y = df[target]
-    
-    
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    
-    cat_processor = OrdinalEncoder()
-    num_processor = MinMaxScaler()
-    
-    num_vals = X._get_numeric_data().columns.tolist()
-    cat_vals = X.select_dtypes("object").columns.tolist()
-    
-    processor = ColumnTransformer(transformers=[("cat", cat_processor, cat_vals), ("num", num_processor, num_vals)])
-    
-    vars = {"chi2":[], "mutual_info":[]}
-    
-    for m in (chi2, mutual_info_classif):
-      for k in range(5, 25):
-    
-        selector = make_pipeline(processor,
-                              SelectKBest(m, k=k))
-    
-        selector.fit(X_train, y_train)
-        if m == chi2:
-          vars["chi2"] += selector.get_feature_names_out().tolist()
-        else:
-          vars["mutual_info"] += selector.get_feature_names_out().tolist()
-    
-    
-    
-    vars_kb = pd.DataFrame({i:pd.Series(j).value_counts() for i,j in vars.items()})
-    
-    vars_kb.index = vars_kb.index.str[5:]
-    if plot:
-        
-        vars_kb.sort_values(by=main_metric, ascending=True).plot(kind="barh",)
-        plt.legend(loc=[0.7, 0.2])
-        plt.title("Feature Importance (chi2 vs mutual information)")
-        plt.show()
-
-    
-    metric = ["chi2", "mutual_info"]
-    
-    
-    criterion = ["gini", "entropy", "log_loss"]
-    
-    
-    vars_rfe = pd.DataFrame()
-    
-    for m in metric:
-    
-      target = "Attrition"
-    
-      X = df[vars_kb[m].sort_values(ascending=False).index.values.tolist()[:num_feat_kbest]]
-      y = df[target]
-    
-      X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    
-      cat_processor = OrdinalEncoder()
-      num_processor = MinMaxScaler()
-    
-      num_vals = X._get_numeric_data().columns.tolist()
-      cat_vals = X.select_dtypes("object").columns.tolist()
-    
-      processor = ColumnTransformer(transformers=[("cat", cat_processor, cat_vals), ("num", num_processor, num_vals)])
-        
-      for c in criterion:
-    
-        selector = make_pipeline(processor,
-                                RFE(DecisionTreeClassifier(criterion=c, random_state=42), n_features_to_select=num_rfe))
-    
-        selector.fit(X_train, y_train)
-    
-        X.columns[selector.named_steps["rfe"].support_].values.tolist()
-    
-        vars_rfe[c+f"_{m}"] = X.columns[selector.named_steps["rfe"].support_].values
-    
-
-
-    counter = Counter()
-
-    for i in vars_rfe.columns:
-      counter.update(vars_rfe[i])
-    
-    
-    n_select = pd.DataFrame(counter.values(), index=counter.keys()).rename(columns={0:"count"}).sort_values(by="count", ascending=True)
-    
-    if plot:
-        n_select.plot(kind="barh", title="Numero de apariciones en los criterios", legend=False)
-        plt.show()
-            
-        
-    
-    return vars_kb, vars_rfe, n_select
-    
-def fit_model(vars=None, year=None, clf="rf", sample=None):
+def fit_model(vars=None, year=None, clf="rf", sample=None, save=False, prefix=""):
 
   if year:
     query = f"SELECT * FROM df WHERE strftime('%Y', Date) = '{year}'"
@@ -418,7 +339,7 @@ def fit_model(vars=None, year=None, clf="rf", sample=None):
 
   y = df[target]
 
-  global X_test, y_test
+
   X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
   if clf == "svc":
@@ -449,7 +370,7 @@ def fit_model(vars=None, year=None, clf="rf", sample=None):
 
     model = make_pipeline(processor,
                       RandomForestClassifier(random_state=42))
-  else:
+  elif clf == "gb":
 
     model = make_pipeline(processor,
                       GradientBoostingClassifier(random_state=42))
@@ -462,9 +383,267 @@ def fit_model(vars=None, year=None, clf="rf", sample=None):
 
   else:
     model.fit(X_train, y_train)
+  
+  if os.path.exists('models'):
+        pass
+  else:
+      os.makedirs(f"models/{prefix}/model_{prefix}")
+
+  to_store = [model]
+
+  for i in to_store:
+      for name, value in locals().items():
+          if i is value:
+              if name == "i":
+                  pass
+  
+              with open(f"models/{prefix}/{name}.pkl", "wb") as f:
+                  pickle.dump(i, f)
+      
 
   return model
+
+def feature_sel(num_feat_kbest=20, num_rfe=15, plot_metric="chi2", plot=False, year_vars=None, sample=None, year_train=2016):
+
+    if year_vars:
+        query = f"SELECT * FROM df WHERE strftime('%Y', Date) = '{year_vars}'"
+    else:
+        query = "SELECT * FROM df"
+
+    df_vars = create_df(query, index=True)
+
+    target = "Attrition"
+
+    X = df_vars.drop([target, "retirementType", "resignationReason", "Date"], axis=1)
+    y = df_vars[target]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    cat_processor = OrdinalEncoder()
+    num_processor = MinMaxScaler()
+
+    num_vals = X._get_numeric_data().columns.tolist()
+    cat_vals = X.select_dtypes("object").columns.tolist()
+
+    processor = ColumnTransformer(transformers=[("cat", cat_processor, cat_vals), ("num", num_processor, num_vals)])
+
+    vars = {"chi2":[], "mutual_info":[]}
+
+    for m in (chi2, mutual_info_classif):
+      for k in range(5, 25):
+
+        selector = make_pipeline(processor,
+                              SelectKBest(m, k=k))
+
+        selector.fit(X_train, y_train)
+        if m == chi2:
+          vars["chi2"] += selector.get_feature_names_out().tolist()
+        else:
+          vars["mutual_info"] += selector.get_feature_names_out().tolist()
+
+
+    vars_kb = pd.DataFrame({i:pd.Series(j).value_counts() for i,j in vars.items()})
+    vars_kb.index = vars_kb.index.str[5:]
+
+    if plot:
+
+        vars_kb.sort_values(by=plot_metric, ascending=True).plot(kind="barh",)
+        plt.legend(loc=[0.7, 0.2])
+        plt.title("Feature Importance (chi2 vs mutual information)")
+        plt.show()
+
+
+    metric = ["chi2", "mutual_info"]
+    criterion = ["gini", "entropy", "log_loss"]
+
+    vars_rfe = pd.DataFrame()
+
+    for m in metric:
+
+      target = "Attrition"
+
+      X = df_vars[vars_kb[m].sort_values(ascending=False).index.values.tolist()[:num_feat_kbest]]
+      y = df_vars[target]
+
+      X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+      cat_processor = OrdinalEncoder()
+      num_processor = MinMaxScaler()
+
+      num_vals = X._get_numeric_data().columns.tolist()
+      cat_vals = X.select_dtypes("object").columns.tolist()
+
+      processor = ColumnTransformer(transformers=[("cat", cat_processor, cat_vals), ("num", num_processor, num_vals)])
+
+      for c in criterion:
+
+        selector = make_pipeline(processor,
+                                RFE(DecisionTreeClassifier(criterion=c, random_state=42), n_features_to_select=num_rfe))
+
+        selector.fit(X_train, y_train)
+
+        X.columns[selector.named_steps["rfe"].support_].values.tolist()
+
+        vars_rfe[c+f"_{m}"] = X.columns[selector.named_steps["rfe"].support_].values
+
+
+
+    counter = Counter()
+
+    for i in vars_rfe.columns:
+      counter.update(vars_rfe[i])
+
+
+    n_select = pd.DataFrame(counter.values(), index=counter.keys()).rename(columns={0:"count"}).sort_values(by="count", ascending=True)
+
+    if plot:
+        n_select.plot(kind="barh", title="Numero de apariciones en los criterios", legend=False)
+        plt.show()
+
+    info = {i:[] for i in vars_rfe.columns}
+    clfs = ["rf", "gb", "svc"]
+
+    if year_train:
+        query = f"SELECT * FROM df WHERE strftime('%Y', Date) = '{year_train}'"
+    else:
+        query = "SELECT * FROM df"
+
+    df_train = create_df(query, index=True)
+
+    for v in vars_rfe:
+        for clf in clfs:
+            vars = vars_rfe[v].values.tolist()
+            target = "Attrition"
+            X = df_train[vars]
+            y = df_train[target]
+            X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+            model = fit_model(vars=vars, clf=clf, sample=sample, year=year_train)
+            pred = model.predict(X_test)
+            info[v].append(f1_score(y_test, pred, pos_label="1"))
+
+    _ = {"recurrente": []}
+    for clf in clfs:
+        vars = n_select.index.values.tolist()
+        X = df_train[vars]
+        y = df_train[target]
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        model = fit_model(vars=vars, clf=clf, sample=sample, year=year_train)
+        pred = model.predict(X_test)
+        _["recurrente"].append(f1_score(y_test, pred, pos_label="1"))
+
+    info.update(_)
+
+    every = {"all_feat": []}
+    for clf in clfs:
+        vars = n_select.index.values.tolist()
+        X = df_train.drop([target, "retirementType", "resignationReason", "Date"], axis=1)
+        y = df_train[target]
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        model = fit_model(vars=vars, clf=clf, sample=sample, year=year_train)
+        pred = model.predict(X_test)
+        every["all_feat"].append(f1_score(y_test, pred, pos_label="1"))
+
+    info.update(every)
     
-    
+    eval_df = pd.DataFrame(info, index=[clfs])
+
+    if os.path.exists('features'):
+        pass
+    else:
+        os.mkdir("features")
+
+    to_store = [vars_kb, vars_rfe, n_select, eval_df]
+
+    for i in to_store:
+      for name, value in locals().items():
+        if i is value:
+            if name == "i":
+                pass
+
+            with open(f"features/{name}.pkl", "wb") as f:
+                 pickle.dump(i, f)    
 
     
+def hyper_tunning(year=2016):
+
+  with open("features/vars_rfe.pkl", "rb") as f:
+      vars_rfe = pickle.load(f)
+
+  param_grid = {"randomforestclassifier__n_estimators":range(25,100,25),
+                "randomforestclassifier__criterion":["gini", "entropy", "log_loss"],
+                "randomforestclassifier__max_features":["sqrt", "log2", None]}
+
+  features = vars_rfe["gini_chi2"].values.tolist()
+
+  if year:
+      query = f"SELECT * FROM df WHERE strftime('%Y', Date) = '{year}'"
+  else:
+      query = "SELECT * FROM df"
+
+  df = create_df(query, index=True)
+  cv = 7
+
+  target = "Attrition"
+
+  X = df[features]
+  y = df[target]
+
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+  cat_processor = OrdinalEncoder()
+  num_processor = StandardScaler()
+
+  num_vals = X._get_numeric_data().columns.tolist()
+  cat_vals = X.select_dtypes("object").columns.tolist()
+
+  processor = ColumnTransformer(transformers=[("cat", cat_processor, cat_vals), ("num", num_processor, num_vals)])
+
+  clf = make_pipeline(processor,
+                      RandomForestClassifier(random_state=42))
+
+  model_tofit = GridSearchCV(clf, param_grid=param_grid, cv=cv, n_jobs=-1, verbose=1)
+
+
+  model_tofit.fit(X_train, y_train)
+
+
+  best_params = {i[len("randomforestclassifier__"):]:j for i,j in model_tofit.best_params_.items()}
+  best_model = model_tofit.best_estimator_
+
+  if os.path.exists('models/best_model'):
+      pass
+  else:
+      os.makedirs("models/best_model")
+
+  to_store = [features, best_params, df, best_model]
+
+  for i in to_store:
+    for name, value in locals().items():
+      if i is value:
+          if name == "i":
+              pass
+
+          with open(f"models/best_model/{name}.pkl", "wb") as f:
+              pickle.dump(i, f)
+
+
+
+def make_prediction(df=None):
+
+    assert isinstance(df, pd.DataFrame), "Se necesita una base de datos para la prediccion"
+
+    with open("models/best_model/features.pkl", "rb") as f:
+        feat = pickle.load(f)
+
+    with open("models/best_model/best_model.pkl", "rb") as f:
+        model = pickle.load(f)
+
+    X = df[feat]
+
+    prediction = model.predict(X)
+
+    pred_df = pd.DataFrame(prediction, index=X.index, columns=["Prediccion"])
+
+    pred_df.to_csv("models/best_model/predictions.csv", index=False)
+
+    return prediction   
